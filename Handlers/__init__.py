549164ca3f6ec416ce75b0f41ad64708fb5e8524
walter-cavinaw <w.cavinaw@gmail.com>
2014-11-01 21:56:30 -0700

    removed any calls to django. There might be too many issues with using django and tornado, such as authorization and database usage.

--- Handlers/__init__.py
+++ Handlers/__init__.py
@@ -1,1 +1,6 @@
-__author__ = 'Walter'
+from AuthHandler import LoginHandler, LogoutHandler
+from BaseHandler import BaseHandler
+from ChartSocketHandler import ChartSocketHandler
+from HomeHandler import HomeHandler
+from MainHandler import MainHandler
+from UserSocketHandler import UserSocketHandler
