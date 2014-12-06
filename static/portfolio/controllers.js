7b5170d65333918fd8c73195393bfa168633de10
Walter <w.cavinaw@gmail.com>
2014-12-05 16:42:13 -0800

    angular stock controller now gets the stock data and has all stock related info inside of it. The stock-chart directive watches for stock data and creates the chart from there.

--- static/portfolio/controllers.js
+++ static/portfolio/controllers.js
@@ -1,57 +1,49 @@
 var portfolio = angular.module("portfolio", []);
 
-portfolio.controller("stockController", function($scope, $http){
+portfolio.controller("StockController", function($scope, $http){
 
-    $scope.init = function(stocks){
-        $scope.stocks = stocks;
+    $scope.init_data = function() {
+        var currentDate = new Date();
+        var currentDateString = currentDate.getFullYear() + "-" + ("0" + (currentDate.getMonth() + 1)).slice(-2) + "-" + ("0" + currentDate.getDate()).slice(-2);
+        var previousDate = new Date();
+        var previousDate = new Date(previousDate.setMonth(currentDate.getMonth() - 3));
+        var previousDateString = previousDate.getFullYear() + "-" + ("0" + (previousDate.getMonth() + 1)).slice(-2) + "-" + ("0" + previousDate.getDate()).slice(-2);
 
-        console.log(stocks);
+        var main = "http://www.quandl.com/api/v1/datasets/WIKI/" + $scope.stock.ticker + ".json";
+        var params = "?&trim_start=" + previousDateString + "&trim_end=" + currentDateString;
+        var auth = "&auth_token=sok7xuv8xDR_9LooZmaZ";
+        var url = main + params + auth;
+        $http.get(url).success(function (response) {
+            var parseDate = d3.time.format("%Y-%m-%d").parse;
+            $scope.stock.data = (response.data).map(function (d) {
+                return {
+                    date: parseDate(d[0]),
+                    open: +d[1],
+                    high: +d[2],
+                    low: +d[3],
+                    close: +d[4],
+                    volume: +d[5]
+                };
+            });
+            var data = $scope.stock.data;
+            // array of all price data
+            var recentData = data[data.length - 1];
 
-        for(stock of stocks){
-            console.log(stock.ticker);
+            var closePrice = recentData.close;
+            var changePrice = Number(recentData.close) - Number(recentData.open);
+            changePrice = changePrice.toFixed(2);
 
-            var currentDate = new Date();
-            var currentDateString = currentDate.getFullYear() + "-" + ("0" + (currentDate.getMonth() + 1)).slice(-2) + "-" + ("0" + currentDate.getDate()).slice(-2);
-            var previousDate = new Date();
-            var previousDate = new Date(previousDate.setMonth(currentDate.getMonth() - 3));
-            var previousDateString = previousDate.getFullYear() + "-" + ("0" + (previousDate.getMonth() + 1)).slice(-2) + "-" + ("0" + previousDate.getDate()).slice(-2);
+            $scope.stock.closePrice = closePrice;
+            $scope.stock.changePrice = changePrice;
 
-            var main = "http://www.quandl.com/api/v1/datasets/WIKI/" + stock.ticker + ".json";
-            var params = "?&trim_start=" + previousDateString + "&trim_end=" + currentDateString;
-            var auth = "&auth_token=sok7xuv8xDR_9LooZmaZ";
-            var url = main+params+auth;
-
-            (function(stock){$http.get(url).success(function(response){
-
-                stock.response = response;
-
-                var data = response.data;
-
-                var closeIndex = response.column_names.indexOf("Close");
-                var openIndex = response.column_names.indexOf("Open");
-
-                // array of all price data
-                var recentData = data[data.length-1];
-
-                var closePrice = recentData[closeIndex];
-                console.log(closePrice);
-                var changePrice = Number(recentData[closeIndex]) - Number(recentData[openIndex]);
-                changePrice = changePrice.toFixed(2);
-                console.log(changePrice);
-
-                stock.closePrice = closePrice;
-                stock.changePrice = changePrice;
-
-                if(changePrice < 0){
-                    stock.changePriceColor = "#CC0000";
-                } else{
-                    stock.changePriceColor = "#00CC00";
-                }
-            })})(stock);
-        }
-
+            if (changePrice < 0) {
+                $scope.stock.changePriceColor = "#CC0000";
+            } else {
+                $scope.stock.changePriceColor = "#00CC00";
+            }
+        });
     };
-
+    $scope.init_data();
 });
 
 portfolio.controller('ListController', function($scope){
@@ -64,21 +56,14 @@ portfolio.directive('stockChart', function(){
     return {
         restrict: 'EA',
         scope: {
-            ticker: '@'
+            ticker: '@',
+            data:'='
         },
-        link: function (scope, element, attrs){
+        link: function (scope, element){
             var margin = {top: 20, right: 20, bottom: 30, left: 50};
             var width = element[0].getBoundingClientRect().width - margin.left - margin.right;
             var height = element[0].getBoundingClientRect().height - margin.top - margin.bottom;
-            var currentDate = new Date;
-            var currentDateString = currentDate.getFullYear() + "-" + ("0" + (currentDate.getMonth() + 1)).slice(-2) + "-" + ("0" + currentDate.getDate()).slice(-2);
-            var previousDate = new Date;
-            var previousDate = new Date(previousDate.setMonth(currentDate.getMonth() - 3));
-            var previousDateString = previousDate.getFullYear() + "-" + ("0" + (previousDate.getMonth() + 1)).slice(-2) + "-" + ("0" + previousDate.getDate()).slice(-2);
-            var query = "http://www.quandl.com/api/v1/datasets/WIKI/" + scope.ticker + ".json";
-            var params = "?&trim_start=" + previousDateString + "&trim_end=" + currentDateString;
-            var auth = "&auth_token=sok7xuv8xDR_9LooZmaZ";
-            var url = query+params+auth;
+
             var svg = d3.select(element[0]).append("svg")   // this needs to be fixed
                     .attr("width", width + margin.left + margin.right)
                     .attr("height", height + margin.top + margin.bottom)
@@ -104,44 +89,36 @@ portfolio.directive('stockChart', function(){
             var yAxis = d3.svg.axis()
                 .scale(y)
                 .orient("left");
-            d3.json(url, function (error, data) {
-                var accessor = close.accessor();
-                data = data.data;
-                data = data.map(function (d) {
-                    return {
-                        date: parseDate(d[0]),
-                        open: +d[1],
-                        high: +d[2],
-                        low: +d[3],
-                        close: +d[4],
-                        volume: +d[5]
-                    };
-                }).sort(function (a, b) {
-                    return d3.ascending(accessor.d(a), accessor.d(b));
-                });
+            var accessor = close.accessor();
+            scope.$watch('data', function(newVal){
+                if (newVal) {
+                    newVal.sort(function (a, b) {
+                        return d3.ascending(accessor.d(a), accessor.d(b));
+                    });
 
-                x.domain(data.map(accessor.d));
-                y.domain(techan.scale.plot.ohlc(data, accessor).domain());
+                    x.domain(newVal.map(accessor.d));
+                    y.domain(techan.scale.plot.ohlc(newVal, accessor).domain());
 
-                svg.append("g")
-                    .datum(data)
-                    .attr("class", "close")
-                    .call(close);
+                    svg.append("g")
+                        .datum(newVal)
+                        .attr("class", "close")
+                        .call(close);
 
-                svg.append("g")
-                    .attr("class", "x axis")
-                    .attr("transform", "translate(0," + height + ")")
-                    .call(xAxis);
+                    svg.append("g")
+                        .attr("class", "x axis")
+                        .attr("transform", "translate(0," + height + ")")
+                        .call(xAxis);
 
-                svg.append("g")
-                    .attr("class", "y axis")
-                    .call(yAxis)
-                    .append("text")
-                    .attr("transform", "rotate(-90)")
-                    .attr("y", 6)
-                    .attr("dy", ".71em")
-                    .style("text-anchor", "end")
-                    .text("Price ($)");
+                    svg.append("g")
+                        .attr("class", "y axis")
+                        .call(yAxis)
+                        .append("text")
+                        .attr("transform", "rotate(-90)")
+                        .attr("y", 6)
+                        .attr("dy", ".71em")
+                        .style("text-anchor", "end")
+                        .text("Price ($)");
+                }
             });
         }
     }
