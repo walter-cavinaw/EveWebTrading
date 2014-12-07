a584baa66aabf166e85d76bd65c521ae9def8bbf
walter-cavinaw <w.cavinaw@gmail.com>
2014-12-06 17:54:50 -0800

    Fix stock query to account for changes in database. The primary key of a stock should be the dataset example (WIKI/AAPL) because realistically it is possible for two different stocks to have the same ticker across different markets and therefore we cant just use the ticker alone.

--- sql_queries/queries.py
+++ sql_queries/queries.py
@@ -1,3 +1,5 @@
 find_user_query = "SELECT * FROM users WHERE email = %s"
 new_user_query = "INSERT INTO users (email, pass) VALUES (%s, %s)"
-folio_stocks_query = "SELECT s.ticker FROM portfolio_stocks s WHERE s.userid=%s and s.folioid=%s"
+folio_stocks_query = "SELECT s.ticker, s.name, s.dataset, s.startdate" \
+                     " FROM portfolio_stocks ps, stocks s " \
+                     "WHERE ps.userid=%s and ps.folioid=%s and ps.dataset = s.dataset"
