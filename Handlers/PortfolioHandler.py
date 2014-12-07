2a2214bed463a52a2b8300b3474ea1a0ce552635
walter-cavinaw <w.cavinaw@gmail.com>
2014-12-06 19:59:30 -0800

    improve the way stocks from the db are returned.

--- Handlers/PortfolioHandler.py
+++ Handlers/PortfolioHandler.py
@@ -1,6 +1,6 @@
 from BaseHandler import BaseHandler
 import logging
-import jsonpickle
+import json
 from sql_queries import queries
 
 class PortfolioHandler(BaseHandler):
@@ -15,9 +15,13 @@ class PortfolioHandler(BaseHandler):
         userEmail = self.get_secure_cookie("user")
 
         if userEmail:
-            stocks = db.query(stocks_query, userEmail, 'default')
+            db_stocks = db.query(stocks_query, userEmail, 'default')
             logging.info(stocks)
         else:
-            stocks = db.query(stocks_query, 'dev@test.com', 'default')
+            db_stocks = db.query(stocks_query, 'dev@test.com', 'default')
+        stocks = json.dumps(db_stocks, default=self.date_handler)
+        self.render("portfolio.html", stocks=stocks, portfolios=portfolios)
 
-        self.render("portfolio.html", jsonpickle=jsonpickle, stocks=stocks, portfolios=portfolios)
+    @staticmethod
+    def date_handler(obj):
+        return obj.isoformat() if hasattr(obj, 'isoformat') else obj
