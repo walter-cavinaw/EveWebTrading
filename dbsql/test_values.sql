69bc39be01f60c5d735e8092d03cb3c3c1113cb3
albertkim <albert275@gmail.com>
2014-12-24 02:31:45 -0800

    Fixed test data query

--- dbsql/test_values.sql
+++ dbsql/test_values.sql
@@ -1,6 +1,11 @@
 insert into users (email, pass)
 values ('dev@test.com', '$2a$12$Cklg4o8PqtbyRlKZM0gMkO0qBCduvQFGd82a4tegtASHaDMkEJ6a.');
 
+insert into stocks values ('COST', 'Costco', 'WIKI/COST', null);
+insert into stocks values ('GOOG', 'Google', 'WIKI/GOOG', null);
+insert into stocks values ('FB', 'Facebook', 'WIKI/FB', null);
+insert into stocks values ('DIS', 'Disneu', 'WIKI/DIS', null);
+
 insert into portfolio_stocks values ('dev@test.com', 'default', 'WIKI/COST');
 insert into portfolio_stocks values ('dev@test.com', 'default', 'WIKI/GOOG');
 insert into portfolio_stocks values ('dev@test.com', 'default', 'WIKI/FB');
